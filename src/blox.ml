fd6449eb198f7f58a9538065a162a5d227f93b0b
Naeem B <sgtb3@users.noreply.github.com>
2016-11-20 21:20:03 -0500

    updated blox.ml

--- src/blox.ml
+++ src/blox.ml
@@ -1,20 +1,35 @@
-(* Top-level of the Blox compiler: scan & parse the input,
-   check the resulting AST, generate AMF output, and dump the module *)
+(* Top-level of the Blox compiler: scan & parse the input, check the resulting 
+  AST, and generate the AMF file as output *)
 
-type action = Ast | LLVM_IR | Compile
+(* Need to get *)
+type action = Usage | Ast | Compile | AMF
+
+let usage = 
+  "\n USAGE: \n" ^
+  " \t  blox.sh [-option] <input_file.blox> \n" ^
+  " OPTIONS: \n" ^
+  " \t  -a   Display the Abstract Syntax Tree\n" ^
+  " \t  -c   Compile input_file.blox to output_file.amf\n"
 
 let _ =
-  let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-a", Ast);	(* Print the AST only *)
-			      ("-l", LLVM_IR);  (* Generate LLVM, don't check *)
-			      ("-c", Compile) ] (* Generate, check LLVM IR *)
-  else Compile in
-  let lexbuf = Lexing.from_channel stdin in
-  let ast = Parser.program Scanner.token lexbuf in
-  Semant.check ast;
+  let action, blox_file = 
+    if Array.length Sys.argv > 1 then
+      List.assoc Sys.argv.(1) 
+      [ ("-a", Ast); ("-c", Compile) ] (* -a: print AST only; -c gen AMF file *)
+    else 
+      Compile 
+  in
+  let file_in_chan = open_in blox_file
+  in
+  let lexbuf = Lexing.from_channel file_in_chan 
+  in
+  let ast = Parser.program Scanner.token lexbuf 
+  in
+  analyzer.check ast;
+  
   match action with
-    Ast -> print_string (Ast.string_of_program ast)
-  | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
-  | Compile -> let m = Codegen.translate ast in
-    Llvm_analysis.assert_valid_module m;
-    print_string (Llvm.string_of_llmodule m)
+    | Usage   ->  print_endline usage
+    | Ast     ->  print_string (Ast.string_of_program ast)
+    | Compile ->  let m = Generator.translate ast 
+                  in
+                  print_string (Generator.string_of_module m)
