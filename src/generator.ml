e16755655168504445cc04c8a01e751c0f449a38
TRW2119 <trw2119@columbia.edu>
2016-12-18 00:52:14 -0500

    2 generation functions
    "generator2" is probably the we need

--- src/generator.ml
+++ src/generator.ml
@@ -1,6 +1,6 @@
-
 open Ast
 open Sast
+open Printf
 
 exception Face_Taken of string;;
 exception Block_Overlap of string;;
@@ -8,11 +8,111 @@ exception Invalid_Face of string;;
 exception Opposite_Face of string;;
 exception Invalid_Block of string;;
 exception Block_Out_Of_Bounds of string;;
+(*
+let x = ref 0 in
+let y = ref 0 in 
+let z = ref 0 in
+let vertices = ref [|0; 1; 2; 3; 4; 5; 6; 7|] in
+let line = ref 8 in
+let generate = function 
+	| Fr_print f1 -> 
+		let _ =						 	  				
+		let oc = open_out "test.amf" in   	(* create or truncate file, return channel *)
+			let top = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<amf>\n\t<object id=\"1\">\n\t\t<mesh>" in 
+			let bottom = "\t\t\t</volume>\n\t\t</mesh>\n\t</object>\n</amf>" in   
+			let vertexstart = "\t\t\t<vertices>" in
+			let vertexend = "\t\t\t</vertices>" in
+			let vpart1 = "<vertex><coordinates><x>" in
+			let vpart2 = "</x><y>" in 
+			let vpart3 = "</y><z>" in
+			let vpart4 = "</z></coordinates></vertex>" in
+			let trianglestart = "\t\t\t<volume>" in
+			let triangleend = "\t\t\t</volume>" in
+			let tpart1 = "<triangle><v1>" in
+			let tpart2 = "</v1><v2>" in
+			let tpart3 = "</v2><v3>" in
+			let tpart4 = "</v3></triangle>" in
+					
+				fprintf oc "%s\n" top;  				(* write top *)
+				fprintf oc "%s\n" vertexstart;  (* write start of vertices *)
+				
+				(* write vertices *)
+				while (!line > 0) do
+					if (!line > 4) then
+						x := f1.x
+					else 
+						x := 0
+					if (!line mod 4 = 0 || !line mod 4 = 3) then
+						y := f1.y 
+					else
+						y := 0
+					if (!line mod 2 = 0) then
+						z := f1.z
+					else 
+						(z := 0)
+					fprintf oc "%s%d%s%d%s%d%s\n" vpart1 x vpart2 y vpart3 z vpart4; (* write each vertex *) 
+					!line = !line - 1;							
+				done						
+				
+				fprintf oc "%s\n" vertexend;	(* write end of vertices *)
+				
+				fprintf oc "%s\n" trianglestart;	(* write start of triangles *)
+				
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(0) tpart2 !vertices.(1) tpart3 !vertices.(3) tpart4; (* write each triangle *) 
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(0) tpart2 !vertices.(2) tpart3 !vertices.(3) tpart4; (* write each triangle *)
+				
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(5) tpart2 !vertices.(6) tpart3 !vertices.(7) tpart4; (* write each triangle *)
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(4) tpart2 !vertices.(5) tpart3 !vertices.(7) tpart4; (* write each triangle *)
+				
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(0) tpart2 !vertices.(1) tpart3 !vertices.(5) tpart4; (* write each triangle *)
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(0) tpart2 !vertices.(4) tpart3 !vertices.(5) tpart4; (* write each triangle *)
+				
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(2) tpart2 !vertices.(3) tpart3 !vertices.(7) tpart4; (* write each triangle *)
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(2) tpart2 !vertices.(6) tpart3 !vertices.(7) tpart4; (* write each triangle *)
+				
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(0) tpart2 !vertices.(2) tpart3 !vertices.(6) tpart4; (* write each triangle *)
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(0) tpart2 !vertices.(4) tpart3 !vertices.(6) tpart4; (* write each triangle *)
+				
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(1) tpart2 !vertices.(3) tpart3 !vertices.(7) tpart4; (* write each triangle *)
+				fprintf oc "%s%d%s%d%s%d%s\n" tpart1 !vertices.(1) tpart2 !vertices.(5) tpart3 !vertices.(7) tpart4; (* write each triangle *)				
+				
+				fprintf oc "%s\n" triangleend;	(* write end of triangles *)
+					
+				fprintf oc "%s\n" bottom;  			(* write bottom *)
+				close_out oc                		(* flush and close the channel *)
+	
+	| _ -> ()
+	
 
+
+	
+*)
 
 let generate (globals, functions) =
+	
   print_endline "Add generator code here ...\n"
 
+	(*
+	
+let print (globals, functions) =
+	if (stmt.Fr_print != "") then 
+		
+		
+	
+		let frame_list = List.fold_left (fun m (t, n) -> String.Map.add n t m)
+	StringMap.empty (fr_decl.fr_name @ stmt.Fr_decl.frame_name)
+	
+	
+	
+	
+	let symbols = List.fold_left (fun m (t, n) -> StringMap.add n t m)
+	StringMap.empty (globals @ func.formals @ func.locals )
+	print_endline "Add generator code here ...\n"
+
+if (hey != "") then print_string "yes" else print_string "no";;
+	
+*)	
+	
 (*
 (* GENERATE AMF FILE CODE BELOW*)
 (* Read in .blox file into a list *)
